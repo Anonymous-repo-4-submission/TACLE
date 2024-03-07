1f90b8f882c6e1d7a9d270de68f5e408c4a3a2e0
Anonymous-repo-4-submission <143321699+Anonymous-repo-4-submission@users.noreply.github.com>
2024-03-07 20:20:37 +0530

    Add files via upload

---
+++ train_CIFAR10.sh
@@ -0,0 +1,30 @@
+##### CIFAR 10 - Imagenet 0.8
+python main.py --config=./exps_cifar10_0.8/slca_cifar10-imagenet_pretrained_0.8%_0Buffer_ourApproach.json
+# python main.py --config=./exps_cifar10_0.8/slca_cifar10-imagenet_pretrained_0.8%_0Buffer_slca.json
+# python main.py --config=./exps_cifar10_0.8/slca_cifar10-imagenet_pretrained_0.8%_0Buffer_slca_fixmatch.json
+
+# ##### CIFAR 10 - Imagenet 5
+# python main.py --config=./exps_cifar10_5/slca_cifar10-imagenet_pretrained_5%_0Buffer_ourApproach.json
+# python main.py --config=./exps_cifar10_5/slca_cifar10-imagenet_pretrained_5%_0Buffer_slca.json
+# python main.py --config=./exps_cifar10_5/slca_cifar10-imagenet_pretrained_5%_0Buffer_slca_fixmatch.json
+
+# ##### CIFAR 10 - Imagenet 25
+# python main.py --config=./exps_cifar10_25/slca_cifar10-imagenet_pretrained_25%_0Buffer_ourApproach.json
+# python main.py --config=./exps_cifar10_25/slca_cifar10-imagenet_pretrained_25%_0Buffer_slca.json
+# python main.py --config=./exps_cifar10_25/slca_cifar10-imagenet_pretrained_25%_0Buffer_slca_fixmatch.json
+
+
+##### CIFAR 10 - moco 0.8
+# python main.py --config=./exps_cifar10_0.8/slca_cifar10-mocov3_pretrained_0.8%_0Buffer_ourApproach.json
+# python main.py --config=./exps_cifar10_0.8/slca_cifar10-mocov3_pretrained_0.8%_0Buffer_slca.json
+# python main.py --config=./exps_cifar10_0.8/slca_cifar10-mocov3_pretrained_0.8%_0Buffer_slca_fixmatch.json
+
+# ##### CIFAR 10 - moco 5
+# python main.py --config=./exps_cifar10_5/slca_cifar10-mocov3_pretrained_5%_0Buffer_ourApproach.json
+# python main.py --config=./exps_cifar10_5/slca_cifar10-mocov3_pretrained_5%_0Buffer_slca.json
+# python main.py --config=./exps_cifar10_5/slca_cifar10-mocov3_pretrained_5%_0Buffer_slca_fixmatch.json
+
+# ##### CIFAR 10 - moco 25
+# python main.py --config=./exps_cifar10_25/slca_cifar10-mocov3_pretrained_25%_0Buffer_ourApproach.json
+# python main.py --config=./exps_cifar10_25/slca_cifar10-mocov3_pretrained_25%_0Buffer_slca.json
+# python main.py --config=./exps_cifar10_25/slca_cifar10-mocov3_pretrained_25%_0Buffer_slca_fixmatch.json
---
+++ train_CIFAR100.sh
@@ -0,0 +1,32 @@
+# ##### CIFAR 100 - Imagenet 0.8
+python main.py --config=./exps_cifar100_0.8/slca_cifar100-imagenet_pretrained_0.8%_0Buffer_ourApproach.json
+# python main.py --config=./exps_cifar100_0.8/slca_cifar100-imagenet_pretrained_0.8%_0Buffer_slca.json
+# python main.py --config=./exps_cifar100_0.8/slca_cifar100-imagenet_pretrained_0.8%_0Buffer_slca_fixmatch.json
+
+##### CIFAR 100 - moco 0.8
+python main.py --config=./exps_cifar100_0.8/slca_cifar100-mocov3_pretrained_0.8%_0Buffer_ourApproach.json
+# python main.py --config=./exps_cifar100_0.8/slca_cifar100-mocov3_pretrained_0.8%_0Buffer_slca.json
+# python main.py --config=./exps_cifar100_0.8/slca_cifar100-mocov3_pretrained_0.8%_0Buffer_slca_fixmatch.json
+
+
+# ##### CIFAR 100 - Imagenet 5
+# python main.py --config=./exps_cifar100_5/slca_cifar100-imagenet_pretrained_5%_0Buffer_ourApproach.json
+# python main.py --config=./exps_cifar100_5/slca_cifar100-imagenet_pretrained_5%_0Buffer_slca.json
+# python main.py --config=./exps_cifar100_5/slca_cifar100-imagenet_pretrained_5%_0Buffer_slca_fixmatch.json
+
+
+# ##### CIFAR 100 - moco 5
+# python main.py --config=./exps_cifar100_5/slca_cifar100-mocov3_pretrained_5%_0Buffer_ourApproach.json
+# python main.py --config=./exps_cifar100_5/slca_cifar100-mocov3_pretrained_5%_0Buffer_slca.json
+# python main.py --config=./exps_cifar100_5/slca_cifar100-mocov3_pretrained_5%_0Buffer_slca_fixmatch.json
+
+
+# ##### CIFAR 100 - Imagenet 25
+# python main.py --config=./exps_cifar100_25/slca_cifar100-imagenet_pretrained_25%_0Buffer_ourApproach.json
+# python main.py --config=./exps_cifar100_25/slca_cifar100-imagenet_pretrained_25%_0Buffer_slca.json
+# python main.py --config=./exps_cifar100_25/slca_cifar100-imagenet_pretrained_25%_0Buffer_slca_fixmatch.json
+
+# ##### CIFAR 100 - moco 25
+# python main.py --config=./exps_cifar100_25/slca_cifar100-mocov3_pretrained_25%_0Buffer_ourApproach.json
+# python main.py --config=./exps_cifar100_25/slca_cifar100-mocov3_pretrained_25%_0Buffer_slca.json
+# python main.py --config=./exps_cifar100_25/slca_cifar100-mocov3_pretrained_25%_0Buffer_slca_fixmatch.json
